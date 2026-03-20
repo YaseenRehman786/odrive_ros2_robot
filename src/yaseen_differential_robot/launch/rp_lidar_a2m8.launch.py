72c16b57914fc10bbafa4921f7d27d0f67e84a0d
test1 <testemail@gmail.com>
2026-03-19 22:53:43 -0400

    Update rplidar launch file

---
+++ src/yaseen_differential_robot/launch/rp_lidar_a2m8.launch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
+    frame_id = LaunchConfiguration('frame_id', default='laser')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+    
+    scan_mode = LaunchConfiguration('scan_mode', default='Express')
+    # main scan modes:
+    #   Standard, Express, Boost, 
+    # additional scan modes:
+    #   Sensitivity, Stability, SingleChannel, ForceSample, ForceScan, ForceSingleScan
+	
+    return LaunchDescription([
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_node',
+            name='rplidar_node',
+            output='screen',
+            parameters=[{
+                'channel_type': channel_type,
+                'serial_port': serial_port,
+                'serial_baudrate': serial_baudrate,
+                'frame_id': frame_id,
+                'scan_mode': scan_mode,
+                'angle_compensate': angle_compensate,
+                'inverted': inverted
+            }]
+        )
+    ])
